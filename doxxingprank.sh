15e91427b9e271e87f35c1517d7985207edf1172
I must be strong <164299950+mestronger@users.noreply.github.com>
2024-03-21 17:33:13 +0700

    Create doxxingprank.sh

---
+++ doxxingprank.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+
+echo "
+█▀▄ █▀█ ▀▄▀ ▀▄▀ █ █▄░█ █▀▀   █▀▄▀█ █▀█ ░
+█▄▀ █▄█ █░█ █░█ █ █░▀█ █▄█   █░▀░█ █▀▄ ▄
+
+█▀█ █▀█ ▄▀█ █▄░█ █▄▀
+█▀▀ █▀▄ █▀█ █░▀█ █░█"
+
+echo "=> Name: Mr. prank"
+
+echo "=> Age: 15 years old"
+
+echo "=> Adress: Kampung melayu, indonesia"
+
+echo "=> Ip adress: 126.255.255.255.14"
+
+echo "=> Emei: 865272889991628"
+
+
+echo "=> Altitute: 6.193125, 106.821810"
+
+echo "=> Device: Smasnug A50s"
+
+echo "=> telekomunication service: PT. kroncongsel Tbk"
